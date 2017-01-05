caadeaf293dbc344faeab7a20a9eedc077a7a77c
Xiaohui Wang <wang502@indiana.edu>
2017-01-04 19:59:00 -0500

    update /examples

---
+++ examples/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+    "strings"
+    "log"
+    "flag"
+    "time"
+    "github.com/wang502/gores/gores"
+    "github.com/wang502/gores/examples/tasks"
+)
+
+func Produce(config *gores.Config){
+    item := map[string]interface{}{
+      "Name": "Rectangle",
+      "Queue": "TestJob",
+      "Args": map[string]interface{}{
+                "Length": 10,
+                "Width": 10,
+              },
+      "Enqueue_timestamp": time.Now().Unix(),
+    }
+
+    resq := gores.NewResQ(config)
+    err := resq.Enqueue(item)
+    if err != nil {
+        log.Fatalf("ERROR Enqueue item to ResQ")
+    }
+}
+
+func Consume(config *gores.Config){
+    tasks := map[string]interface{}{
+                "Item": tasks.PrintItem,
+                "Rectangle": tasks.CalculateArea,
+           }
+    gores.Launch(config, &tasks)
+}
+
+func main()  {
+    configPath := flag.String("c", "config.json", "path to configuration file")
+    option := flag.String("o", "consume", "option: weither produce or consume")
+    flag.Parse()
+
+    config, err := gores.InitConfig(*configPath)
+    if err != nil {
+        log.Fatalf("Cannot read config file")
+    }
+
+    if strings.Compare("produce", *option) == 0 {
+        Produce(config)
+    } else {
+        Consume(config)
+    }
+}
